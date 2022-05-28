6f3a3f5010e70060160b3fa83c8a4d3cdbcc6829
marieono <104805318+marieono@users.noreply.github.com>
2022-05-29 08:34:39 +0900

    画像ファイルを入れた

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ export const Header = () => {
     const componentName = () => "Header";
     return (
         <div className="test-wrapper">
-            このコンポーネントの名前は{componentName()}です。
+            <p className="text">
+                このコンポーネントの名前は{componentName()}です。
+            </p>
         </div>//ここはHTMLではなく、JSX
     );
 };
