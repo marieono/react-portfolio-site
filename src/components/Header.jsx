c8b48fb31f38fb0834d239f40db9f52dca8c7706
marieono <104805318+marieono@users.noreply.github.com>
2022-05-29 09:34:56 +0900

    スタイルが上手くあてられない

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,29 @@
+import CoverImage from '../images/cover-image.jpeg';
+import ProfileImage from '../images/profile-image.jpg';
+
 export const Header = () => {
-    const componentName = () => "Header";
     return (
-        <div className="test-wrapper">
-            <p className="text">
-                このコンポーネントの名前は{componentName()}です。
-            </p>
-        </div>//ここはHTMLではなく、JSX
+        <header className="main-cover" style={{ backgroundimage: `url(${CoverImage})` }}>
+            {/* ovrlayはカバー画像の上に透過して表示される背景要素 */}
+            <div className="overlay"></div>
+            <div className="container">
+                <div className="display-table">
+                    <div className="display-table-contents">
+                        {/* カバー画像 */}
+                        <div className="profile-thumb" style={{ backgroundImage: `url(${ProfileImage})` }}></div>
+                        <h1 className="title-text">まり</h1>
+                        <h3 className="title-text">学生</h3>
+                        <ul className="social-icons">
+                            <li className="icon-link">
+                                {/* TODO */}
+                            </li>
+                            <li className="icon-link">
+                                {/* TODO */}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 };
