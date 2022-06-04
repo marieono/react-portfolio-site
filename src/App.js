6a23c356b24b07ee415b3e6bcb532fe401131359
marieono <104805318+marieono@users.noreply.github.com>
2022-06-05 08:43:35 +0900

    Contact Usのボタンができない

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
 import { Header } from './components/Header';
+import { Skills } from './components/Skills';
+import { Contact } from './components/Contact';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Skills />
+      <Contact />
+    </>
   );
 }
 
