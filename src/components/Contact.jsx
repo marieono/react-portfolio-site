1d0ca47c723cd571b1f295369b034cb3a1bd335b
marieono <104805318+marieono@users.noreply.github.com>
2022-06-12 08:10:19 +0900

    useReducerで実装したらしい

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ export const Contact = () => {
                 <div className="contact-contents">
                     <h2>Contact to Me!</h2>
                     <p className="description">お仕事のご相談・ご依頼など下記よりご連絡ください。</p>
-                    <a href="https://twitter.com/" className="btn">Contact Us</a>
+                    <a href="https://twitter.com/erimano23" className="btn">Contact Us</a>
                 </div>
             </div>
         </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import axios from 'axios';
+import { skillReducer, initialState, actionTypes } from "../reducers/skillReducer";
 
 export const Skills = () => {
-    useEffect(() => { axios.get('https://api.github.com/users/marieono/repos').then((response) => console.log(response)) }, []);
+    const [state, dispatch] = useReducer(skillReducer, initialState);
+
+    useEffect(() => {
+        dispatch({ type: actionTypes.fetch });
+        axios.get('https://api.github.com/users/marieono/repos')
+            .then((response) => {
+                const languageList = response.data.map(res => res.language);
+                const countedLanguageList = generateLanguageConuntObj(languageList);
+                dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
+            })
+            .catch(() => {
+                dispatch({ type: actionTypes.error });
+            });
+    }, []);
+
+    const generateLanguageConuntObj = (allLanguageList) => {
+        const notNullLanguageList = allLanguageList.filter(language => language != null);
+        const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+        return uniqueLanguageList.map(item => {
+            return {
+                language: item,
+                count: allLanguageList.filter(language => language === item).length
+            }
+        });
+    };
 
     return (
         <div id="skills">
