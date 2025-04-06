aba2ae0f0ab1b81eb5e8d39d98391758d2249f46
marieono <104805318+marieono@users.noreply.github.com>
2025-04-06 20:02:05 +0900

    学生

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,34 +3,40 @@ import CoverImage from '../images/cover-image.jpeg';
 import ProfileImage from '../images/profile-image.jpg';
 
 export const Header = () => {
-    return (
-        <header className="main-cover" style={{ backgroundImage: `url(${CoverImage})` }}>
-            {/* ovrlayはカバー画像の上に透過して表示される背景要素 */}
-            <div className="overlay"></div>
-            <div className="container">
-                <div className="display-table">
-                    <div className="display-table-contents">
-                        {/* カバー画像 */}
-                        <div className="profile-thumb" style={{ backgroundImage: `url(${ProfileImage})` }}></div>
-                        <h1 className="title-text">まり</h1>
-                        <h3 className="title-text">学生</h3>
-                        <ul className="social-icons">
-                            <li className="icon-link">
-                                {/* TODO */}
-                                <a href="https://twitter.com/erimano23">
-                                    <FaTwitter color="white" size="2rem" />
-                                </a>
-                            </li>
-                            <li className="icon-link">
-                                {/* TODO */}
-                                <a href="https://github.com/marieono">
-                                    <FaGithub color="white" size="2rem" />
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header
+      className="main-cover"
+      style={{ backgroundImage: `url(${CoverImage})` }}
+    >
+      {/* ovrlayはカバー画像の上に透過して表示される背景要素 */}
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            {/* カバー画像 */}
+            <div
+              className="profile-thumb"
+              style={{ backgroundImage: `url(${ProfileImage})` }}
+            ></div>
+            <h1 className="title-text">まり</h1>
+            <h3 className="title-text">社会人</h3>
+            <ul className="social-icons">
+              <li className="icon-link">
+                {/* TODO */}
+                <a href="https://twitter.com/erimano23">
+                  <FaTwitter color="white" size="2rem" />
+                </a>
+              </li>
+              <li className="icon-link">
+                {/* TODO */}
+                <a href="https://github.com/marieono">
+                  <FaGithub color="white" size="2rem" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
